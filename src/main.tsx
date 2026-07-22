3a3a88a70d0f50e5cc698ff50f44d91e8a3183bb
danfelito <danfelavicas@gmail.com>
2026-07-21 22:43:25 -0600

    Initialize hero portrait mouse motion

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { startHeroPointerMotion } from './heroPointerMotion';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+
+const stopHeroPointerMotion = startHeroPointerMotion();
+
+if (import.meta.hot) {
+  import.meta.hot.dispose(stopHeroPointerMotion);
+}
